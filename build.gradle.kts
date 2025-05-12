c09086e9efaaa3a84fafa331637f4867fcd5cc2f
JoseLuis-S <shjose@iesrafaelalberti.es>
2025-05-12 10:05:35 +0200

    Dependencias añadidas

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.10"
 }
 
-group = "prog2425.dam1.prueba-calc-propio"
+group = "entornos2425.dam1.task_manager_test"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,8 +10,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 
 tasks.test {
     useJUnitPlatform()
