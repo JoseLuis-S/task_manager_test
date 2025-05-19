393c754a7bdc3ed562a98ea77e8565677d0391dd
JoseLuis-S <shjose@iesrafaelalberti.es>
2025-05-19 09:54:16 +0200

    Segundo error ktlint arreglado

--- src/main/kotlin/servicios/ActividadService.kt
+++ src/main/kotlin/servicios/ActividadService.kt
@@ -6,8 +6,9 @@ import es.prog2425.taskmanager.modelo.Estado
 import es.prog2425.taskmanager.modelo.Tarea
 import es.prog2425.taskmanager.modelo.Evento
 
-class ActividadService(private val repositorio: IActividadRepository) {
-
+class ActividadService(
+    private val repositorio: IActividadRepository,
+) {
     fun crearEvento(descripcion: String, fecha: String, ubicacion: String) {
         val evento = Evento.crearInstancia(descripcion, fecha, ubicacion)
         repositorio.agregarEvento(evento)
